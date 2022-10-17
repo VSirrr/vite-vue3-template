f9460f71c7a939d7b92489c20631c48686f22adb
魏建军 <weijj@yiche.com>
2022-10-17 17:45:53 +0800

    feat: add aos demo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import AOS from 'aos'
 import './style.scss'
+import 'aos/dist/aos.css'
 import App from './App.vue'
 import router from './router'
+import { createApp } from 'vue'
+
+AOS.init()
 
 createApp(App).use(router).mount('#app')
--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -2,6 +2,34 @@
 
 <template>
   <div>Plan Page</div>
+  <div class="aos-all">
+    <div data-id="1" class="aos-item" data-aos="fade-up"></div>
+    <div data-id="2" class="aos-item" data-aos="fade-down"></div>
+    <div data-id="3" class="aos-item" data-aos="zoom-out-down"></div>
+    <div data-id="4" class="aos-item" data-aos="flip-down"></div>
+    <div data-id="5" class="aos-item" data-aos="flip-up"></div>
+    <div data-id="6" class="aos-item" data-aos="fade-down"></div>
+    <div data-id="7" class="aos-item" data-aos="fade-in"></div>
+    <div data-id="8" class="aos-item" data-aos="fade-down"></div>
+    <div data-id="9" class="aos-item" data-aos="fade-in"></div>
+    <div data-id="10" class="aos-item" data-aos="fade-down"></div>
+    <div data-id="11" class="aos-item" data-aos="fade-up"></div>
+    <div data-id="12" class="aos-item" data-aos="fade-down"></div>
+    <div data-id="13" class="aos-item" data-aos="fade-in"></div>
+    <div data-id="14" class="aos-item" data-aos="fade-up"></div>
+    <div data-id="15" class="aos-item" data-aos="fade-in"></div>
+    <div data-id="16" class="aos-item" data-aos="fade-up"></div>
+    <div data-id="17" class="aos-item" data-aos="fade-down"></div>
+    <div data-id="18" class="aos-item" data-aos="fade-up"></div>
+    <div data-id="19" class="aos-item" data-aos="zoom-out"></div>
+    <div data-id="20" class="aos-item" data-aos="fade-up"></div>
+    <div data-id="21" class="aos-item" data-aos="zoom-out"></div>
+    <div data-id="22" class="aos-item" data-aos="fade-in"></div>
+    <div data-id="23" class="aos-item" data-aos="zoom-out-up"></div>
+    <div data-id="24" class="aos-item" data-aos="zoom-out-down"></div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import '/src/styles/aos';
+</style>
