17c4824bab3562c2959e71d24a357c53e3f0a77a
魏建军 <weijj@yiche.com>
2022-11-21 16:24:49 +0800

    chore: add rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,5 +8,7 @@ module.exports = {
     'no-empty-source': null,
     'selector-class-pattern': null,
     'color-function-notation': null,
+    'keyframes-name-pattern': null,
+    'value-no-vendor-prefix': null,
   },
 }
